948bb712b413578b8c35257680f52fb6c433be71
mikebutts <mgbutts@gmail.com>
2017-10-01 20:13:42 -0400

    added ability to add books, project done

--- src/App.js
+++ src/App.js
@@ -6,30 +6,47 @@ import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts : []
+    contacts: []
   }
   componentDidMount() {
     ContactsAPI.getAll().then((contacts) => {
       this.setState({ contacts })
     })
   }
-    removeContact = (contact) => {
-      this.setState((state)=> ({
-        contacts: state.contacts.filter((c) => c.id !== contact.id)
-      }))
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
 
-      ContactsAPI.remove(contact)
-    }
-    
+    ContactsAPI.remove(contact)
+  }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
-      <div className="app">
-        <Route exact path="/" render={() => (
-          <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} />
-        )} />
-        <Route path="/create" component={CreateContact} />
+      <div>
+        <Route exact path='/' render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+        )}/>
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
-
     )
   }
 }
